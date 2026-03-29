c49569735592c5f963e0ca0b0ed5fe38c27139c7
Cooper Ericksen <devcoopa@gmail.com>
2026-03-28 20:54:06 -0700

    Added motion hover animations to nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,52 @@
+import { motion } from 'motion/react';
+
 import '../styles/Nav.css';
 
 function Nav() {
     return (
         <nav>
-            <a className="nav-link" id="nav-logo-container" href="/"><img id="nav-logo" src="./logo.png" alt="coopericksen's logo" /></a>
-            <a className="nav-link" href="/">Home</a>
-            <a className="nav-link" href="/tools">Tools</a>
-            <a className="nav-link" href="/projects">Projects</a>
-            <a className="nav-link" href="/about">About</a>
+            <motion.a
+                className="nav-link" 
+                id="nav-logo-container" 
+                href="/"
+
+                whileHover={{ scale: 1.2, y: -5 }}
+                whileTap={{ scale: 0.95 }}
+            >
+                <img id="nav-logo" src="./logo.png" alt="coopericksen's logo" />
+            </motion.a>
+
+            <motion.a 
+                className="nav-link" 
+                href="/"
+
+                whileHover={{ scale: 1.2, y: -5 }}
+                whileTap={{ scale: 0.95 }}
+            >Home</motion.a>
+
+            <motion.a 
+                className="nav-link" 
+                href="/tools"
+
+                whileHover={{ scale: 1.2, y: -5 }}
+                whileTap={{ scale: 0.95 }}
+            >Tools</motion.a>
+
+            <motion.a 
+                className="nav-link" 
+                href="/projects"
+
+                whileHover={{ scale: 1.2, y: -5 }}
+                whileTap={{ scale: 0.95 }}
+            >Projects</motion.a>
+
+            <motion.a 
+                className="nav-link" 
+                href="/about"
+
+                whileHover={{ scale: 1.2, y: -5 }}
+                whileTap={{ scale: 0.95 }}
+            >About</motion.a>
         </nav>
     )
 }
