494ffea596b34bf93bba93b89867b915cef0454e
Cooper Ericksen <devcoopa@gmail.com>
2026-04-01 13:19:27 -0700

    Added motion animations to LiveClock component

--- src/pages/tools/Clock.tsx
+++ src/pages/tools/Clock.tsx
@@ -1,9 +1,20 @@
+import { motion } from 'motion/react';
+
 import LiveClock from "../../components/LiveClock";
 
 function Clock() {
     return (
         <>
-            <LiveClock />
+            <motion.section
+                style={{ width: "max-content", margin: "0 auto" }}
+
+                initial={{ scale: 0, y: -50 }} 
+                animate={{ scale: 1, y: 0, transition: { delay: 1.4, duration: 0.2 } }}
+
+                whileHover={{ scale: 1.05, y: -20, rotate: 2}}
+            >
+                <LiveClock />
+            </motion.section>
         </>
     )
 }
