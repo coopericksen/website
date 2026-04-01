8b58f32f0b4d8d185010a75ebd45f555516085ee
Cooper Ericksen <devcoopa@gmail.com>
2026-04-01 13:57:41 -0700

    Fixed LiveClock animation on IndexPage

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,14 +1,37 @@
+import { motion } from 'motion/react';
+
 import Nav from '../components/Nav';
 import WelcomeBanner from '../components/WelcomeBanner';
 import LiveClock from '../components/LiveClock';
 import Footer from '../components/Footer';
 
 function IndexPage() {
+    const LiveClock_container_motion = {
+        hidden: { opacity: 0 },
+        visible: { 
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.2,
+                delayChildren: 1.4
+            }
+        },
+    }
+
     return (
         <>
             <Nav />
             <WelcomeBanner />
-            <LiveClock />
+
+            <motion.section
+                variants={LiveClock_container_motion}
+                initial="hidden"
+                whileInView="visible"
+            >
+                <a href="/tools/clock" style={{ textDecoration: "none" }}>
+                    <LiveClock />
+                </a>
+            </motion.section>
+
             <Footer />
         </>
     )
