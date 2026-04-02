ad54b7f43ad0e107a69c69baddc7b9b9e099491d
Cooper Ericksen <coopericksengit config --global user.email coopericksen@gmail.com>
2026-04-02 01:22:08 -0700

    Updated footer appearance and added draggable element

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,50 @@
+import { motion } from 'motion/react';
+import { useRef } from 'react';
+
 import '../styles/Footer.css';
 
 function Footer() {
+    const dragConstraintRef = useRef(null);
+    
     return(
-        <footer>
-            <div className='footer-elgroup'>
-                <h4>Links</h4>
-                <a href="https://github.com/coopericksen/website" target='_blank'>Github Repository</a>
-                <a href="https://legacy.coopericksen.com" target='_blank'>Old Site</a>
+        <footer ref={dragConstraintRef}>
+            <div className="footer-fixed">
+                <div className="footer-content">
+                    <div className='footer-elgroup'>
+                        <h4>Links</h4>
+                        <a href="https://github.com/coopericksen/website" target='_blank'>Github Repository</a>
+                        <a href="https://legacy.coopericksen.com" target='_blank'>Old Site</a>
+                    </div>
+
+                    <div className="footer-nav">
+                        <a href="/">Home</a>
+                        <a href="/tools">Tools</a>
+                        <a href="/projects">Projects</a>
+                        <a href="/about">About</a>
+                    </div>
+                </div>
+
+                <motion.h4 
+                    className='footer-author'
+
+                    initial={{ scale: 0 }}
+                    animate={{ scale: 1 }}
+                    transition={{ delay: 2 }}
+                    
+                    drag
+                    dragConstraints={dragConstraintRef}
+                    // dragConstraints={{
+                    //     top: -50,
+                    //     right: 80,
+                    //     bottom: 50,
+                    //     left: -80
+                    // }}
+                    dragElastic={0}
+                >
+                    coopericksen.com
+                </motion.h4>
             </div>
 
-            <div className="footer-nav">
-                <a href="/">Home</a>
-                <a href="/tools">Tools</a>
-                <a href="/projects">Projects</a>
-                <a href="/about">About</a>
-            </div>
         </footer>
     )
 }
