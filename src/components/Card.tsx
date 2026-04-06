a1f68078b938eaec56ef76bfb4807ccf7a4f5c2c
Cooper Ericksen <devcoopa@gmail.com>
2026-04-06 15:03:53 -0700

    Updated Card component to use correct motion variant

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'motion/react';
+import { Card_Variants, Card_Hover } from '../variants/variants';
 
 import { type Project } from "../types/types";
 
@@ -6,24 +7,13 @@ import '../styles/Card.css';
 
 function Card(props: Project) {
 
-    const card_motion = {
-        hidden: {
-            scale: 0,
-            y: -50
-        },
-        visible: {
-            scale: 1,
-            y: 0
-        }
-    }
-
     return (
         <motion.a 
             className="card" 
             href={props.href}
 
-            variants={card_motion}
-            whileHover={{ scale: 1.05, y: -20, rotate: 2 }}
+            variants={Card_Variants}
+            whileHover={Card_Hover}
         >
             <h4>{props.title}</h4>
             <p>{props.description}</p>
