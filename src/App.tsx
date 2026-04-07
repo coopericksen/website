9e9d8cae41c215df4d2628c804a196097e314dc6
Cooper Ericksen <devcoopa@gmail.com>
2026-04-06 23:05:15 -0700

    Added Vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import IndexPage from './pages/Index';
 import ToolsPage from './pages/Tools';
