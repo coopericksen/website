ee5a7e92d88c694d558803a45f2803643b7a164d
Cooper Ericksen <devcoopa@gmail.com>
2026-04-06 22:56:05 -0700

    Fixed Vercel analytics import statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import IndexPage from './pages/Index';
 import ToolsPage from './pages/Tools';
